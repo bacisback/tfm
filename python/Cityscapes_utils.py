3347b43bcf48b51e4994d8be70343dca751b0378
Tu Nombre <you@example.com>
2021-04-06 13:07:12 +0200

    hu

--- python/Cityscapes_utils.py
+++ python/Cityscapes_utils.py
@@ -119,9 +119,9 @@ def parse_label():
                 img_name = os.path.join(data_dir, img_name)
                 f.write("{},{}.npy\n".format(img_name, lab_name))
 
-                if os.path.exists(lab_name + '.npy'):
-                    print("Skip %s" % (filename))
-                    continue
+                #if os.path.exists(lab_name + '.npy'):
+                #    print("Skip %s" % (filename))
+                #    continue
                 print("Parse %s" % (filename))
                 img = os.path.join(city_dir, filename)
                 img = np.asarray(Image.open(img).convert('RGB'))
@@ -136,7 +136,7 @@ def parse_label():
                             idx_mat[h, w] = index
                         except:
                             # no index, assign to void
-                            idx_mat[h, w] = 19
+                            idx_mat[h, w] = 0
                 idx_mat = idx_mat.astype(np.uint8)
                 np.save(lab_name, idx_mat)
                 print("Finish %s" % (filename))
